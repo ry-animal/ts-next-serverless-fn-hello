d7efd8b62aa08f441e3f45c95df58d03ed78e934
Ryan Van Valkenburg <ryanlvv@gmail.com>
2023-03-20 20:29:50 -0700

    Add [name].ts API route to handle requests

---
+++ src/app/api/[name].ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+ 
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const { name } = request.query;
+  return response.end(`Hello ${name}!`);
+}
